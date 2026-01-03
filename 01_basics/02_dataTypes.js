e78e2248ecde4174376e40c6c475a37467ecb423
SaritaKumari24 <128452745+SaritaKumari24@users.noreply.github.com>
2026-01-03 09:01:44 +0000

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treate all js code as newer verison
+
+// alert("hello") //we are using nodejs, not browser
+
+console.log(3+3);
+ console.log("sarita")
+
+ //number=>2 to power 53
+ //bigint =>
+//string=>""
+//  boolean=>true/false
+// null=>standalone value
+// undefined=>
+    //symbol=>unique
+
+//object
+
+let age=18
+let isLoggedIn= false
+let state=null
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object

04f97bad88c27d75ec758312d6b97a36e7d896c4
SaritaKumari24 <128452745+SaritaKumari24@users.noreply.github.com>
2026-01-03 08:46:23 +0000

    variable done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,19 @@
+const accountId = 144553
+
+let accountEmail = "sarita@gmail.com"
+
+var accountPassword ="12345"
+
+accountCity= "jaipur"
+
+let accountState;
+
+// accountId=3
+// console.log(accountId);
+
+// accountEmail="neha@gmail.in"
+// console.log(accountEmail);
+
+accountPassword="987654345"
+accountCity="bihar"
+console.table([accountPassword, accountCity,accountState])

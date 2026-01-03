1799194d1f57a79db450e412c8ccb2409b419150
SaritaKumari24 <128452745+SaritaKumari24@users.noreply.github.com>
2026-01-03 10:21:43 +0000

    data type summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,38 @@
+// #primitive
+
+// 7 types: String, Number, boolean, null, undefined,symbol, bigInt
+
+const score=100
+const scoreValue=100.3
+const isLoggedIn = false
+
+const outsideTemp=null
+
+
+let userEmail;
+
+const id=Symbol('1234');
+const anotherId=Symbol('123');
+
+console.log(id === anotherId);
+
+const bigNumber= 34567898765434567n
+
+//reference type(non primitive)
+
+//Array, objects, function
+
+
+const heros = ["shaktiman","naagraj","doga"] //array
+
+
+//objects
+let myObj={
+    name:"sarita",
+    age:22
+}
+
+//function
+const myFunction=function(){
+    console.log("hello world");
+}

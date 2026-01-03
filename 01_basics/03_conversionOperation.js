edd0d7d432b1f887a2fa2286ca596254a414d01c
SaritaKumari24 <128452745+SaritaKumari24@users.noreply.github.com>
2026-01-03 09:12:22 +0000

    conversion done

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,15 @@
+let score ="33dfg"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33"=>33
+// "33sdf"=>NaN
+
+let isLoggedIn=1
+let booleanIsLoogedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoogedIn);

1f83c4ede7797fe711ebd504969f8020680082bc
SaritaKumari24 <128452745+SaritaKumari24@users.noreply.github.com>
2026-01-03 10:01:57 +0000

    conversion operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -13,3 +13,21 @@ console.log(valueInNumber);
 let isLoggedIn=1
 let booleanIsLoogedIn=Boolean(isLoggedIn)
 console.log(booleanIsLoogedIn);
+
+// ****************Operations***************
+
+let value=3
+let negValue=-value
+
+console.log(negValue)
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+console.log(+true);
+
+let num1, num2, num3
+num1=num2=num3=2+2
+console.log(num1)
